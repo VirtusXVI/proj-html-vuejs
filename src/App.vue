615c282cb139639a0ff6d710238eae83901003ed
VirtusXVI <manuel.aveta2001@gmail.com>
2022-07-15 15:37:24 +0200

    checkpoint 1

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
+    <MyHeader/>
   </div>
 </template>
 
 <script>
-
+import MyHeader from "./components/MyHeader.vue"
 export default {
   name: 'App',
   components: {
+    MyHeader,
   }
 }
 </script>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="header-background">
+        <div class="header-container">
+            <section class="header-content">
+                
+            </section>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "../assets/scss/common.scss";
+    .header-background{
+        background-image: url(../assets/img/home-background-hero-scaled.jpg);
+        background-size: contain;
+        background-repeat: no-repeat;
+    }
+</style>
